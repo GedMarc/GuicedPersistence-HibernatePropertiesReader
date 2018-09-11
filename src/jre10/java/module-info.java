df7509ebefc8d178d753176f82a37ed739dff5be
GedMarc <ged_marc@hotmail.com>
2018-09-11 14:05:12 +0200

    first commit

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,13 @@
+import com.jwebmp.guicedpersistence.readers.hibernateproperties.HibernateDefaultConnectionBaseBuilder;
+import com.jwebmp.guicedpersistence.readers.hibernateproperties.HibernateEntityManagerProperties;
+import com.jwebmp.guicedpersistence.services.PropertiesConnectionInfoReader;
+import com.jwebmp.guicedpersistence.services.PropertiesEntityManagerReader;
+
+module com.jwebmp.guicedpersistence.readers.hibernateproperties {
+	requires com.jwebmp.guicedpersistence;
+	exports com.jwebmp.guicedpersistence.readers.hibernateproperties;
+
+	provides PropertiesConnectionInfoReader with HibernateDefaultConnectionBaseBuilder;
+	provides PropertiesEntityManagerReader with HibernateEntityManagerProperties;
+
+}
