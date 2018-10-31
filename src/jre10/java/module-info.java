d30171fd66f10fbeda4cf065fbdd5764df55bc2e
GedMarc <ged_marc@hotmail.com>
2018-11-01 01:10:30 +0200

    Transitive dependencies don't filter down, opening the requested module seems to work great

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -4,12 +4,9 @@ import com.jwebmp.guicedpersistence.services.PropertiesConnectionInfoReader;
 import com.jwebmp.guicedpersistence.services.PropertiesEntityManagerReader;
 
 module com.jwebmp.guicedpersistence.readers.hibernateproperties {
-	requires transitive com.jwebmp.guicedpersistence;
+	requires com.jwebmp.guicedpersistence;
 
 	exports com.jwebmp.guicedpersistence.readers.hibernateproperties;
-
-	requires transitive org.hibernate.orm.core;
-	requires transitive org.hibernate.validator;
 
 	provides PropertiesConnectionInfoReader with HibernateDefaultConnectionBaseBuilder;
 	provides PropertiesEntityManagerReader with HibernateEntityManagerProperties;
