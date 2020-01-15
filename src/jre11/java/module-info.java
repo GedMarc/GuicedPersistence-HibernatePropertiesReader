d71e58f5064c134e00cd9915cd5c79e1bc04871b
GedMarc <ged_marc@hotmial.com>
2020-01-15 19:43:53 +0200

    Font Awesome Pro Icons Collection by Default for enumerations Various Updates and a few safety checks

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,6 +3,8 @@ module com.guicedee.guicedpersistence.readers.hibernateproperties {
 	requires com.guicedee.guicedinjection;
 	requires java.validation;
 
+	requires transitive org.hibernate.orm.core;
+
 	exports com.guicedee.guicedpersistence.readers.hibernateproperties;
 
 	provides com.guicedee.guicedpersistence.services.IPropertiesConnectionInfoReader with com.guicedee.guicedpersistence.readers.hibernateproperties.HibernateDefaultConnectionBaseBuilder;
