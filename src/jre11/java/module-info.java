e47461d21bfa2916d2b10e72f05f5470f3208503
GedMarc <ged_marc@hotmial.com>
2020-03-02 10:28:10 +0200

    Module Linting

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -10,6 +10,4 @@ module com.guicedee.guicedpersistence.readers.hibernateproperties {
 	provides com.guicedee.guicedpersistence.services.IPropertiesConnectionInfoReader with com.guicedee.guicedpersistence.readers.hibernateproperties.HibernateDefaultConnectionBaseBuilder;
 	provides com.guicedee.guicedpersistence.services.IPropertiesEntityManagerReader with com.guicedee.guicedpersistence.readers.hibernateproperties.HibernateEntityManagerProperties;
 
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.guicedee.guicedpersistence.readers.hibernateproperties.HibernateModuleExclusions;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.guicedee.guicedpersistence.readers.hibernateproperties.HibernateModuleExclusions;
-}
+	}
